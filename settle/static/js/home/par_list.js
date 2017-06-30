f580f80a76d2433145c2250bd429006bf8ea4b96
chujunfang <chujunfang@qfpay.com>
2017-06-30 17:18:05 +0800

    dd

---
+++ settle/static/js/home/par_list.js
@@ -0,0 +1,12 @@
+'use strict';
+
+require(['../require-config'], function () {
+    require(["zepto"], function ($) {
+        $(function () {
+            $('.js_more').on('click', function () {
+                var new_list = '<li><dl><dt><img src="http://img3.imgtn.bdimg.com/it/u=3749875149,3961380760&fm=23&gp=0.jpg"></dt><dd><span class="name">小草</span><span class="tel">155****3333</span><span class="price orange">+￥240.0</span></dd><dd class="grey">2017-5-7 17:13:45</dd></dl></li>';
+                $('.js_list').append(new_list);
+            });
+        });
+    });
+});
---
+++ settle/static/js/home/transfer.js
@@ -0,0 +1,47 @@
+'use strict';
+
+require(['../require-config'], function () {
+    require(["zepto"], function ($) {
+        $(function () {
+            $('.js_get_entycode').on('click', function () {
+                $('.js_get_entycode').hide();
+                $('.js_show_entycode').show();
+                timedCount();
+
+                $('.js_yanzhengmsg').text('用户名 15896589658');
+            });
+
+            $('.js_show_entycode').on('click', function () {
+                $('.alert_con').show();
+                $('.alert_con .alert_con_br').html("一分钟后再次获取！");
+                $('.zheceng').show();
+            });
+
+            $('.js_submit').on('click', function () {
+                var tel = $('.js_tel').val();
+                var money = $('.js_money').val();
+                $('.alert_con').show();
+                $('.alert_con .alert_con_br').html('您确认将' + money + '元转入以下账户？<br/>' + tel);
+                $('.zheceng').show();
+            });
+
+            $('.js_alert_con_close').on('click', function () {
+                $('.alert_con').hide();
+                $('.alert_con .alert_con_br').html();
+                $('.zheceng').hide();
+            });
+        });
+    });
+});
+
+function timedCount() {
+    var time0 = $('.js_show_entycode i').text();
+    $('.js_show_entycode i').text(time0 - 1);
+    var t = setTimeout("timedCount()", 1000);
+    if (time0 == 0) {
+        clearTimeout(t);
+        $('.js_get_entycode').show();
+        $('.js_show_entycode i').text(61);
+        $('.js_show_entycode').hide();
+    }
+}
